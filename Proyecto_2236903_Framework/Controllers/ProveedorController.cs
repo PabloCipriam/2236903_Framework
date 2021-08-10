015ef9a457b3f0c79a53fd9ebd279490a8cc7122
Pablo Piñeros <pablopineros2203@gmail.com>
2021-08-10 13:11:28 -0500

    Añadiendo el CRUD de la tabla proveedor

--- Proyecto_2236903_Framework/Controllers/ProveedorController.cs
+++ Proyecto_2236903_Framework/Controllers/ProveedorController.cs
@@ -22,5 +22,100 @@ namespace Proyecto_2236903_Framework.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(proveedor proveedor)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    db.proveedor.Add(proveedor);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch(Exception ex)
+            {
+                ModelState.AddModelError("", "error" + ex);
+                return View();
+            }
+        }
+
+        public ActionResult Details(int id)
+        {
+            using (var db = new inventario2021Entities())
+            {
+                var findProv = db.proveedor.Find(id);
+                return View(findProv);
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    var findProv = db.proveedor.Find(id);
+                    db.proveedor.Remove(findProv);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch(Exception ex)
+            {
+                ModelState.AddModelError("", "error" + ex);
+                return View();
+            }
+        }
+
+        public ActionResult Edit(int id)
+        {
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    proveedor findProv = db.proveedor.Where(a => a.id == id).FirstOrDefault();
+                    return View(findProv);
+
+                }
+            }
+            catch(Exception ex)
+            {
+                ModelState.AddModelError("", "error" + ex);
+                return View();
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(proveedor editProv)
+        {
+            try
+            {
+                using(var db = new inventario2021Entities())
+                {
+                    proveedor Prov = db.proveedor.Find(editProv.id);
+
+                    Prov.nombre = editProv.nombre;
+                    Prov.direccion = editProv.direccion;
+                    Prov.telefono = editProv.telefono;
+                    Prov.nombre_contacto = editProv.nombre_contacto;
+
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch(Exception ex)
+            {
+                ModelState.AddModelError("", "error" + ex);
+                return View();
+            }
+        }
     }
 }
