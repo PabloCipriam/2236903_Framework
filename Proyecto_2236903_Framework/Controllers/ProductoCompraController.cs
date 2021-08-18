6d621e32a3986b3d41c4e6962c494fb382c9ec7a
Pablo Piñeros <pablopineros2203@gmail.com>
2021-08-18 13:13:42 -0500

    Añadiendo CRUD a las tablas con llave foranea usuariorol y producto_compra

---
+++ Proyecto_2236903_Framework/Controllers/ProductoCompraController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Proyecto_2236903_Framework.Models;
+
+namespace Proyecto_2236903_Framework.Controllers
+{
+    public class ProductoCompraController : Controller
+    {
+        // GET: ProductoCompra
+        public ActionResult Index()
+        {
+            using(var db = new inventario2021Entities())
+            {
+                return View(db.producto_compra.ToList());
+            }
+        }
+
+        public static string NombreProducto(int idProducto)
+        {
+            using(var db = new inventario2021Entities())
+            {
+                return db.producto.Find(idProducto).nombre;
+            }
+        }
+
+        public ActionResult ListarProducto()
+        {
+            using(var db = new inventario2021Entities())
+            {
+                return PartialView(db.producto.ToList());
+            }
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(producto_compra producto_Compra)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            try
+            {
+                using(var db = new inventario2021Entities())
+                {
+                    db.producto_compra.Add(producto_Compra);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch(Exception ex)
+            {
+                ModelState.AddModelError("", "error" + ex);
+                return View();
+            }
+        }
+
+        public ActionResult Details(int id)
+        {
+            using(var db = new inventario2021Entities())
+            {
+                return View(db.producto_compra.Find(id));
+            }
+        }
+
+        public ActionResult Edit(int id)
+        {
+            try
+            {
+                using(var db = new inventario2021Entities())
+                {
+                    producto_compra producto_CompraEdit = db.producto_compra.Where(a => a.id == id).FirstOrDefault();
+                    return View(producto_CompraEdit);
+                }
+            }
+            catch(Exception ex)
+            {
+                ModelState.AddModelError("", "error" + ex);
+                return View();
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(producto_compra producto_CompraEdit)
+        {
+            try
+            {
+                using(var db = new inventario2021Entities())
+                {
+                    producto_compra oldproducto_Compra = db.producto_compra.Find(producto_CompraEdit.id);
+
+                    oldproducto_Compra.id_compra = producto_CompraEdit.id_compra;
+                    oldproducto_Compra.id_producto = producto_CompraEdit.id_producto;
+                    oldproducto_Compra.cantidad = producto_CompraEdit.cantidad;
+
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch(Exception ex)
+            {
+                ModelState.AddModelError("", "error" + ex);
+                return View();
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                using(var db = new inventario2021Entities())
+                {
+                    producto_compra producto_Compra = db.producto_compra.Find(id);
+                    db.producto_compra.Remove(producto_Compra);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch(Exception ex)
+            {
+                ModelState.AddModelError("", "error" + ex);
+                return View();
+            }
+        }
+    }
+}
---
+++ Proyecto_2236903_Framework/Controllers/UsuarioRolController.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Proyecto_2236903_Framework.Models;
+
+namespace Proyecto_2236903_Framework.Controllers
+{
+    public class UsuarioRolController : Controller
+    {
+        // GET: UsuarioRol
+        public ActionResult Index()
+        {
+            using(var db = new inventario2021Entities())
+            {
+                return View(db.usuariorol.ToList());
+            }
+        }
+
+        public static string NombreUsuario(int idUsuario)
+        {
+            using(var db = new inventario2021Entities())
+            {
+                return db.usuario.Find(idUsuario).nombre;
+            }
+        }
+
+        public static string DescripcionRol(int idRol)
+        {
+            using(var db = new inventario2021Entities())
+            {
+                return db.roles.Find(idRol).descripcion;
+            }
+        }
+
+        public ActionResult ListarUsuarios()
+        {
+            using(var db = new inventario2021Entities())
+            {
+                return PartialView(db.usuario.ToList());
+            }
+        }
+
+        public ActionResult ListarRoles()
+        {
+            using(var db = new inventario2021Entities())
+            {
+                return PartialView(db.roles.ToList());
+            }
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(usuariorol usuariorol)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            try
+            {
+                using(var db = new inventario2021Entities())
+                {
+                    db.usuariorol.Add(usuariorol);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch(Exception ex)
+            {
+                ModelState.AddModelError("", "error" + ex);
+                return View();
+            }
+        }
+
+        public ActionResult Details(int id)
+        {
+            using(var db = new inventario2021Entities())
+            {
+                return View(db.usuariorol.Find(id));
+            }
+        }
+
+        public ActionResult Edit(int id)
+        {
+            try
+            {
+                using(var db = new inventario2021Entities())
+                {
+                    usuariorol usuariorolEdit = db.usuariorol.Where(a => a.id == id).FirstOrDefault();
+                    return View(usuariorolEdit);
+                }
+            }
+            catch(Exception ex)
+            {
+                ModelState.AddModelError("", "error" + ex);
+                return View();
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(usuariorol usuariorolEdit)
+        {
+            try
+            {
+                using(var db = new inventario2021Entities())
+                {
+                    usuariorol oldusuariorol = db.usuariorol.Find(usuariorolEdit.id);
+
+                    oldusuariorol.idUsuario = usuariorolEdit.idUsuario;
+                    oldusuariorol.idRol = usuariorolEdit.idRol;
+
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch(Exception ex)
+            {
+                ModelState.AddModelError("", "error" + ex);
+                return View();
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                using(var db = new inventario2021Entities())
+                {
+                    usuariorol usuariorol = db.usuariorol.Find(id);
+                    db.usuariorol.Remove(usuariorol);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch(Exception ex)
+            {
+                ModelState.AddModelError("", "error" + ex);
+                return View();
+            }
+        }
+    }
+}
